e7ba1ebe9bff0c9d09f979bd38e564bd78e079ee
inalvare <inigo.alvarez-hernandez@capgemini.com>
2022-03-16 18:28:16 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LetrasPipe } from './letras.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetrasPipe
   ],
   imports: [
     BrowserModule,
